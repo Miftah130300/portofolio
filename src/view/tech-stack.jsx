40d73fc98f1e8cfa2c62a17d339d3617b9ef6f3b
Miftah Fauzy <136819861+Miftah130300@users.noreply.github.com>
2024-01-23 12:02:18 +0700

    Add files via upload

--- src/view/tech-stack.jsx
+++ src/view/tech-stack.jsx
@@ -4,11 +4,12 @@ import javascript from '../asset/js.png'
 import reactjs from '../asset/logo192.png'
 import figma from '../asset/figma.png'
 import tailwind from '../asset/tailwindcss.png'
+import nextjs from '../asset/nextjs.png'
 
 export function TechStack() {
   return (
-    <section className="bg-white h-full w-full min-h-[852px]" id='techstack'>
-      <div className="grid max-w-[350px] md:max-w-[600px] mt-[67px] justify-center mx-auto w-full">
+    <section className="bg-white h-full w-full min-h-[852px] mb-10" id='techstack'>
+      <div className="flex flex-col max-w-[350px] md:max-w-[600px] mt-[67px] justify-center mx-auto w-full">
         <h1 className="text-blue-950 font-bold font-['Poppins'] text-[20px] xl:text-[30px] mx-auto m-5 mt-[50px]">Tech Stack and Skills</h1>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 w-full h-full max-h-[800px] max-w-[500px] rounded-[20px] bg-white shadow-xl p-5 mt-[67px]">
           <div className='flex flex-col justify-center items-center'>
@@ -34,7 +35,11 @@ export function TechStack() {
           <div className='flex flex-col justify-center items-center'>
             <img className='m-5 h-full w-full max-w-[100px] max-h-[100px] p-3' src={figma} alt='figma logo'/>
             <label className='font-bold text-blue-950'>Figma</label>
-          </div>          
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <img className='m-5 h-full w-full max-w-[100px] max-h-[100px] p-3' src={nextjs} alt='nextjs logo'/>
+            <label className='font-bold text-blue-950'>NextJs</label>
+          </div>            
         </div>
       </div>
     </section>
